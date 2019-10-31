b034caa0431e2337aa1b4ac4fce3140555755222
rajan-gith <rajan.prajapati@berylsystems.com>
2019-10-31 19:12:01 +0530

    css added

--- src/Demo.js
+++ src/Demo.js
@@ -2,10 +2,11 @@ import React, {
   Component
 } from 'react';
 import ReactDOM from 'react-dom';
+import "./Demo.css"
 
 class Demo extends Component {
   render() {
-    return <div>
+    return <div className="main-div">
       <h1> It worked...{this.props.name} </h1>
       <p>Enclosed in div tag</p>
     </div>
