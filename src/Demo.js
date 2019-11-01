3d11d41496090b073ed943829bf2020d887b1117
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-01 14:37:50 +0530

    name props accessed differently

--- src/Demo.js
+++ src/Demo.js
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 import "./Demo.css"
 
 
-const Demo = (props) => {
+const Demo = ({name}) => {
 	return <div className="main-div">
-      <h1> It worked...{props.name} </h1>
+      <h1> It worked...{name} </h1>
       <p>Enclosed in div tag</p>
     </div>
 }
