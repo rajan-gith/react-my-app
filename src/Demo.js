02d013e220b9bba4e4300b35957ceaa7c1b87230
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-01 14:35:18 +0530

    funtion based component

--- src/Demo.js
+++ src/Demo.js
@@ -4,13 +4,21 @@ import React, {
 import ReactDOM from 'react-dom';
 import "./Demo.css"
 
-class Demo extends Component {
-  render() {
-    return <div className="main-div">
-      <h1> It worked...{this.props.name} </h1>
+
+const Demo = (props) => {
+	return <div className="main-div">
+      <h1> It worked...{props.name} </h1>
       <p>Enclosed in div tag</p>
     </div>
-  }
 }
+
+// class Demo extends Component {
+//   render() {
+//     return <div className="main-div">
+//       <h1> It worked...{this.props.name} </h1>
+//       <p>Enclosed in div tag</p>
+//     </div>
+//   }
+// }
 
 export default Demo;
